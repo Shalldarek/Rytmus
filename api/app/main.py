67b3e89b8fcb0c432a89a1e14acc88fc28224bcf
Venca Srubar <vaclavcarlosrubar@gmail.cz>
2026-06-04 14:50:51 +0200

    Add delete endpoint

--- api/app/main.py
+++ api/app/main.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from fastapi import Depends, FastAPI
 from api.app import database, models, schemas
 from sqlalchemy.orm import Session
@@ -21,12 +23,20 @@ def get_db():
 def main_menu():
     return "Welcome to Rytmus!"
 
-@app.get("/logs")
-def get_logs(db: Session = Depends(get_db)):
-    logs = db.query(models.DailyLog).all()
-    return logs
+@app.get("/logs/", response_model=list[schemas.DailyLogResponse])
+def get_all_logs(db: Session = Depends(get_db)):
+    return db.query(models.DailyLog).order_by(models.DailyLog.log_date.desc()).all()
 
 @app.get("/logs/{log_date}", response_model=schemas.DailyLogResponse)
 def get_log(log_date: str, db: Session = Depends(get_db)):
     log = db.query(models.DailyLog).filter(models.DailyLog.log_date == log_date).first()
     return log
+
+def delete_daily_log(db: Session, log_date: date):
+    db_log = db.query(models.DailyLog).filter(models.DailyLog.log_date == log_date).first()
+    if db_log:
+        db.delete(db_log)
+        db.commit()
+        return True
+    return False
+
