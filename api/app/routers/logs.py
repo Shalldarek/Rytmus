1d7c4f9cb42e6501ecffeb28466bd04f33c39713
Venca Srubar <vaclavcarlosrubar@gmail.cz>
2026-06-05 10:43:46 +0200

    Add PUT endpoint + tested

--- api/app/routers/logs.py
+++ api/app/routers/logs.py
@@ -50,3 +50,24 @@ def delete_daily_log(db: Session, log_date: date):
         db.commit()
         return True
     return False
+
+@router.put("/{log_date}", response_model=schemas.DailyLogResponse)
+def put_record(log_date: date, log_db: schemas.DailyLogCreate, db: Session=Depends(get_db)):
+    log = db.query(models.DailyLog).filter(models.DailyLog.log_date == log_date).first()
+
+    if log is None:
+        return "No log with such a date in DB"
+    
+    log.sleep_hours = log_db.sleep_hours
+    log.night_awakenings = log_db.night_awakenings
+    log.coffees = log_db.coffees
+    log.stress_level = log_db.stress_level
+    log.water_liters = log_db.water_liters
+    log.screen_time_hours = log_db.screen_time_hours
+    log.workout = log_db.workout
+    log.mood_level = log_db.mood_level
+    
+    db.commit()
+    db.refresh(log)
+
+    return log
